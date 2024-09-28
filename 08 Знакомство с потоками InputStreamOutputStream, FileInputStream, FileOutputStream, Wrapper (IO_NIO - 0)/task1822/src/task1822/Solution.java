abcd9638a35309336f5a23b1334cc0fd0efedd3e
Yury_Home_PC <yury.connect@gmail.com>
2024-09-28 23:01:37 +0300

    solution: task1822

--- 08 Знакомство с потоками InputStreamOutputStream, FileInputStream, FileOutputStream, Wrapper (IO_NIO - 0)/task1822/src/task1822/Solution.java
+++ 08 Знакомство с потоками InputStreamOutputStream, FileInputStream, FileOutputStream, Wrapper (IO_NIO - 0)/task1822/src/task1822/Solution.java
@@ -1,9 +1,8 @@
 package task1822;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Set;
+import java.util.TreeSet;
 
 /* 
 Поиск данных внутри файла
@@ -36,7 +35,62 @@ Requirements:
 3. Программа должна найти в файле и вывести информацию о id, который передается первым параметром.
 4. Поток для чтения из файла должен быть закрыт.*/
 
+
 public class Solution {
+    private static final String PATH = System.getProperty("user.dir") + "/08 Знакомство с потоками InputStreamOutputStream, FileInputStream, FileOutputStream, Wrapper (IO_NIO - 0)/task1822/";
+
     public static void main(String[] args) {
+        BufferedReader consoleReader = null;
+        String fileName;
+        int id;
+        BufferedReader fileReader = null;
+        try {
+            consoleReader = new BufferedReader(new InputStreamReader(System.in));
+
+            System.out.println("\nВведите имя файла с данными.\n\t"
+                    + "ПОДСКАЗКА: Сейчас нужно ввести 'input_data.txt'.");
+            fileName = "input_data.txt"; // использую для тестирования
+//            fileName = consoleReader.readLine().trim();
+
+            System.out.println("Введите id искомого товара: ");
+            id = 195; // использую для тестирования
+//            id = Integer.parseInt(consoleReader.readLine().trim());
+
+            File file = new File(PATH + fileName);
+            fileReader = new BufferedReader(new FileReader(file));
+
+            String line;
+            while ((line = fileReader.readLine()) != null) {
+                String[] lexemes = line.split("\\s+");
+                int idCurrent = Integer.parseInt(lexemes[0]);
+                if (idCurrent == id) {
+                    System.out.printf("\nНайдено совпадение по id = '%d':\n\t%s\n",id, line);
+                }
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+
+        } catch (NumberFormatException e) {
+            System.out.println("Не корректный формат id. GПервой лексемой в строке должна идти числовая величина.");
+            throw new RuntimeException(e);
+
+        } finally {
+            if (fileReader != null) {
+                try {
+                    fileReader.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            if (consoleReader != null) {
+                try {
+                    consoleReader.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 }
