dcda830eb209c4f6d7a92dcee82beadba1a49215
Yury_Home_PC <yury.connect@gmail.com>
2024-09-30 13:13:27 +0300

    solution: task1907

--- 09 Знакомство с потоками ReaderWriter, FileReaderFileWriter, Adapter (IO_NIO - 1)/task1907/src/task1907/Solution.java
+++ 09 Знакомство с потоками ReaderWriter, FileReaderFileWriter, Adapter (IO_NIO - 1)/task1907/src/task1907/Solution.java
@@ -1,9 +1,6 @@
 package task1907;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 /* 
 Считаем слово
@@ -21,6 +18,36 @@ Requirements:
 5. Программа должна выводить в консоль количество слов "world", которые встречаются в файле.*/
 
 public class Solution {
+
+    private static final String SEARCH_TERM = "world";
+
     public static void main(String[] args) {
+        System.out.println("\nКорень проекта 'user.dir' = \t" + System.getProperty("user.dir"));
+        final String path = "09 Знакомство с потоками ReaderWriter, FileReaderFileWriter, Adapter (IO_NIO - 1)/task1907/";
+        System.out.println("Относительный путь 'path' = \t" + path); // выведет текущий ОТНОСИТЕЛЬНЫЙ путь от корня проекта
+        System.out.print("\nВведите имя файла (источник).\t// ПОДСКАЗКА: Сейчас нужно ввести '1.txt' :   ");
+
+        try (BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+             FileReader fileReader = new FileReader(path + consoleReader.readLine().trim());
+             BufferedReader bufferedReader = new BufferedReader(fileReader)) { // hресурсы будут закрыты автоматически в обратном порядке их открытия
+
+            int counter = 0;
+            String line;
+            while ((line = bufferedReader.readLine()) != null) { // вычитаем все строки
+                String noPunctuation = line.replaceAll("[\\p{Punct}]", " "); // Заменяет все знаки препинания на пробелы
+                String singleSpase = noPunctuation.replaceAll("\\s+", " ").trim(); // Заменяет все последовательности пробелов на один пробел.
+                String[] lexemes = singleSpase.split("\\s+");
+                for (String lexeme : lexemes) {
+                    if (lexeme.equals(SEARCH_TERM)) {
+                        counter++;
+                    }
+                }
+            }
+            System.out.printf("\nКоличество слов '%s' встречается в заданном тексте '%d' раз.", SEARCH_TERM, counter);
+
+        } catch (IOException e) {
+            System.out.println("Произошла непредвиденная ошибка во время ввода с клавиатуры");
+            e.printStackTrace();
+        }
     }
 }
